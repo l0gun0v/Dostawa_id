da286317e8534b2c7ad5d8c8c708dc53114ec657
Denis Logunov <dzianis_.lahunou@student.uj.edu.pl>
2022-06-05 17:54:09 +0200

    table

---
+++ target/classes/Data/SQLBase/insert.sql
@@ -0,0 +1,36 @@
+insert into Loginy_hasla values(-1, 'EatEat', getHash('123qweasd'));
+insert into Loginy_hasla values(-1, 'TimeLord', getHash('kl12jh34'));
+insert into Loginy_hasla values(-1, '12345678', getHash('12345678'));
+
+insert into Loginy_hasla values(-1, 'MacDonalds', getHash('secretPassworD'));
+insert into Loginy_hasla values(-1, 'BurgerKing', getHash('BiGwopper'));
+insert into Loginy_hasla values(-1, 'Makaron', getHash('spageTTi9'));
+
+insert into Loginy_hasla values(-1, 'FastDeliver', getHash('FastDeliver'));
+insert into Loginy_hasla values(-1, 'RunWithYourFood', getHash('12345678'));
+insert into Loginy_hasla values(-1, 'Just', getHash('t8klou6'));
+
+insert into Statusy values(1, 'Order placed');
+insert into Statusy values(2, 'Accepted');
+insert into Statusy values(3, 'In process');
+insert into Statusy values(4, 'Waiting for the courier');
+insert into Statusy values(5, 'Dilivery');
+insert into Statusy values(6, 'Delivered');
+
+insert into Kategorie values(1, 'FastFood');
+insert into Kategorie values(2, 'Burger');
+insert into Kategorie values(3, 'Kebab');
+insert into Kategorie values(4, 'Spagetti');
+insert into Kategorie values(5, 'Drink');
+insert into Kategorie values(6, 'Sushi');
+insert into Kategorie values(7, 'Pizza');
+
+insert into Transport_kurjera values(1, 'Car');
+insert into Transport_kurjera values(2, 'Motobake');
+insert into Transport_kurjera values(3, 'Bake');
+
+insert into Kurjery values(800000000, 1, 'Mikolaj', 'Sankewicz', 8800553535, 'fastdeliver@gmail.com');
+insert into Kurjery values(800000001, 2, 'Kasia', 'Samsonawa', 8800553535, 'dostawapl@gmail.com');
+insert into Kurjery values(800000002, 3, 'Andzej', 'Protasewicz', 8800553535, 'deliveryclubr@gmail.com');
+
+
